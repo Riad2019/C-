ecd9875b34f218942733b30121161b9cdb892108
Md. Abdur Rahman <46405817+Riad2019@users.noreply.github.com>
2019-12-23 19:34:39 +0600

    Create hello.cs

---
+++ hello.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace HelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+           name = Console.ReadLine();
+            Console.WriteLine("Hello "+ name);
+            Console.ReadLine();
+        }
+    }
+}
