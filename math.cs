bda0f45c79bcf7a49c991a04c46d79e7a8dd9609
Md. Abdur Rahman <46405817+Riad2019@users.noreply.github.com>
2020-01-22 03:09:40 +0600

    Create math.cs

---
+++ math.cs
@@ -0,0 +1,10 @@
+int a = 7;
+int b = 4;
+int c = 3;
+int d = (a + b) / c;
+int e = (a + b) % c;
+Console.WriteLine($"quotient: {d}");   //quotient: 3
+Console.WriteLine($"remainder: {e}");  //remainder: 2
+
+int f = a + b * c;
+Console.WriteLine(f);       //19
