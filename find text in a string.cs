f24bd33d923b466d74ac10674956594e3900549c
Md. Abdur Rahman <46405817+Riad2019@users.noreply.github.com>
2020-01-22 02:54:29 +0600

    Create find text in a string.cs

---
+++ find text in a string.cs
@@ -0,0 +1,3 @@
+string songLyrics = "You say goodbye, and I say hello";
+Console.WriteLine(songLyrics.Contains("goodbye"));                //True
+Console.WriteLine(songLyrics.Contains("greetings"));              //  False
