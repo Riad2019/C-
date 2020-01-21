082ecaa6dcda9912dfb4a6e99cd656f7747f377a
Md. Abdur Rahman <46405817+Riad2019@users.noreply.github.com>
2020-01-22 02:59:36 +0600

    Update find text in a string.cs

--- find text in a string.cs
+++ find text in a string.cs
@@ -2,3 +2,11 @@ string songLyrics = "You say goodbye, and I say hello";
 Console.WriteLine(songLyrics.Contains("goodbye"));                //True
 Console.WriteLine(songLyrics.Contains("greetings"));              //  False
   //The Contains method returns a boolean value which tells you if the string you were searching for was found. A boolean stores either a true or a false value.
+
+
+string songLyrics = "You say goodbye, and I say hello";
+Console.WriteLine(songLyrics.StartsWith("You"));         //True
+Console.WriteLine(songLyrics.StartsWith("goodbye"));     //False
+
+Console.WriteLine(songLyrics.EndsWith("hello"));        //True
+Console.WriteLine(songLyrics.EndsWith("goodbye"));     //False
