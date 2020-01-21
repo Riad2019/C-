2a4beeb84cdd80c60060d65c8440366a235649ca
Md. Abdur Rahman <46405817+Riad2019@users.noreply.github.com>
2020-01-22 02:55:54 +0600

    Update find text in a string.cs
    The Contains method returns a boolean value which tells you if the string you were searching for was found. A boolean stores either a true or a false value.

--- find text in a string.cs
+++ find text in a string.cs
@@ -1,3 +1,4 @@
 string songLyrics = "You say goodbye, and I say hello";
 Console.WriteLine(songLyrics.Contains("goodbye"));                //True
 Console.WriteLine(songLyrics.Contains("greetings"));              //  False
+  //The Contains method returns a boolean value which tells you if the string you were searching for was found. A boolean stores either a true or a false value.
